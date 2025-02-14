249eda96760778c50c4455c080ed8a67cbe7b838
kahlflekzy <felixsigit@gmail.com>
2025-02-14 10:57:07 +0100

    Update FetchVulnerabilitiesAPI to work with the 2.0 version of the nvd.nist database API.

--- vulnerabilities/views.py
+++ vulnerabilities/views.py
@@ -8,24 +8,25 @@ from .serializers import VulnerabilitySignatureSerializer
 
 class FetchVulnerabilitiesAPI(APIView):
     def get(self, request):
-        api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+        api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0?resultsPerPage=200"
         response = requests.get(api_url)
 
         if response.status_code == 200:
             data = response.json()
             vulnerabilities = []
-            for item in data.get("result", {}).get("CVE_Items", []):
+            for item in data.get("vulnerabilities", []):
+                cve_data = item.get("cve", {})
                 vuln = {
-                    "name": item.get("cve", {}).get("CVE_data_meta", {}).get("ID", "Unknown"),
-                    "description": item.get("cve", {}).get("description", {}).get("description_data", [{}])[0].get(
-                        "value", ""),
-                    "severity": item.get("impact", {}).get("baseMetricV3", {}).get("cvssV3", {}).get("baseSeverity",
-                                                                                                     "Unknown"),
+                    "name": cve_data.get("id", "Unknown"),
+                    "description": cve_data.get("descriptions", [{}])[0].get("value", ""),
+                    "severity": cve_data.get("metrics", {}).get("cvssMetricV31", [{}])[0].get("cvssData", {}).get(
+                        "baseSeverity", "Unknown"),
                     "affected_component": "General",
                     "pattern": "N/A",
                 }
                 vulnerabilities.append(vuln)
-
+            import pprint
+            pprint.pprint(vulnerabilities)
             return Response(vulnerabilities, status=status.HTTP_200_OK)
 
         return Response({"error": "Failed to fetch data"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
