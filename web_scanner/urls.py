c4e7ad362bcb20bad737d161ce11ea4496b0670b
kahlflekzy <felixsigit@gmail.com>
2025-02-14 10:22:52 +0100

    Include vulnerabilities urls.

--- web_scanner/urls.py
+++ web_scanner/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scanner/', include("scanner.urls")),
+    path('api/', include('vulnerabilities.urls')),
 ]
 
