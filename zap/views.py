dee3ead6d5c9b6eaaed926f6523c03c1d3bbdc19
kahlflekzy <felixsigit@gmail.com>
2025-02-26 13:38:36 +0100

    Redirect to home page and show error message on exception.

--- zap/views.py
+++ zap/views.py
@@ -14,16 +14,20 @@ def home(request):
 def scan(request):
     """"""
     url = request.POST.get('url')
-    scan_id = int(request.session.get("zap_scan_id", 0))
-    if scan_id != 0:
+    scan_id = int(request.session.get("zap_scan_id", -1))
+    if scan_id != -1:
         request.session['zap_error'] = ("A scan is currently ongoing, or hasn't been cleared. "
                                           "Please check the scan status.")
         request.session.modified = True
         return redirect("zap:home")
 
-    request.session['zap_scan_id'] = scan_id
+    scan_id, message = spider(url)
+    if scan_id < 0:
+        request.session['zap_error'] = message
+        request.session.modified = True
+        return redirect("zap:home")
+
     request.session['url'] = url
-    scan_id = spider(url)
     request.session['zap_scan_id'] = scan_id
     request.session.modified = True
 
