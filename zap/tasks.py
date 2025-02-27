c5eee86602f2886f39aabc3864a5605802f877ad
kahlflekzy <felixsigit@gmail.com>
2025-02-27 12:36:00 +0100

    Add ajaxSpider.

--- zap/tasks.py
+++ zap/tasks.py
@@ -14,3 +14,22 @@ def spider(target_url):
         return int(scan_id), ""
     except ProxyError:
         return -1, "Scanner was unable to connect to proxy."
+
+
+def ajax_spider(target_url):
+    """
+    When this function is called, do
+    >>> timeout = time.time() * 60 * 2 # 2 minutes from now
+    Then
+
+    >>> while zap.ajaxSpider.status == 'running':
+            if time.time() > timeout:
+                break
+            print('Ajax Spider status' + zap.ajaxSpider.status)
+            time.sleep(2)
+    """
+    try:
+        scan_id = zap.ajaxSpider.scan(target_url)
+        return int(scan_id), ""
+    except ProxyError:
+        return -1, "Scanner was unable to connect to proxy."
