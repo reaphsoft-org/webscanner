bdc38bf6f329807f67983794b24b0f7e581bf2e3
kahlflekzy <felixsigit@gmail.com>
2025-03-04 10:56:39 +0100

    Create the CVE model for fetching the NIST dataset.

--- zap/models.py
+++ zap/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+
+
+class CVE(models.Model):
+    cve_id = models.CharField(max_length=20, unique=True)
+    published_date = models.DateTimeField()
+    last_modified_date = models.DateTimeField()
+    descriptions = JSONField()  # Stores multiple language descriptions
+    metrics = JSONField()  # CVSS scores and other metrics
+    weaknesses = JSONField()  # CWE identifiers
+    references = JSONField()  # List of reference URLs
+    configurations = JSONField()  # Affected products/configurations
+
+    def __str__(self):
+        return self.cve_id
