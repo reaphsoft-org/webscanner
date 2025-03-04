56a73cfd3d4a5f0765b997aa5f03e392808d5559
kahlflekzy <felixsigit@gmail.com>
2025-03-04 13:10:47 +0100

    Update models.py

--- zap/models.py
+++ zap/models.py
@@ -9,20 +9,23 @@ class CVE(models.Model):
     vuln_status = models.CharField(max_length=50)
     published_date = models.DateTimeField()
     last_modified_date = models.DateTimeField()
+
     evaluator_comment = models.TextField(blank=True, null=True)
     evaluator_solution = models.TextField(blank=True, null=True)
     evaluator_impact = models.TextField(blank=True, null=True)
-    descriptions = models.JSONField()  # List of descriptions in various languages
-    metrics = models.JSONField(blank=True, null=True)  # CVSS scores and other metrics
-    weaknesses = models.JSONField(blank=True, null=True)  # CWE identifiers
-    configurations = models.JSONField(blank=True, null=True)  # Affected products/configurations
-    references = models.JSONField(blank=True, null=True)  # List of reference URLs
+
     cisa_exploit_add = models.DateField(blank=True, null=True)
     cisa_action_due = models.DateField(blank=True, null=True)
     cisa_required_action = models.TextField(blank=True, null=True)
     cisa_vulnerability_name = models.CharField(max_length=255, blank=True, null=True)
-    tags = models.JSONField(blank=True, null=True)  # Additional tags
-    vendor_comments = models.JSONField(blank=True, null=True)  # Additional tags
+
+    cve_tags = models.JSONField(blank=True, null=True)  # Only cve_tags is included
+    descriptions = models.JSONField()  # List of descriptions in various languages
+    references = models.JSONField(blank=True, null=True)  # List of reference URLs
+    metrics = models.JSONField(blank=True, null=True)  # CVSS scores and other metrics
+    weaknesses = models.JSONField(blank=True, null=True)  # CWE identifiers
+    configurations = models.JSONField(blank=True, null=True)  # Affected products/configurations
+    vendor_comments = models.JSONField(blank=True, null=True)  # Vendor comments
 
     def __str__(self):
         return self.cve_id
