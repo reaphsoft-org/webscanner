ac12eba354609c9b977205a2bb4ea1f855999fab
kahlflekzy <felixsigit@gmail.com>
2025-03-12 15:31:06 +0100

    Check for cvssMetricV3 in cves.

--- zap/tasks.py
+++ zap/tasks.py
@@ -77,7 +77,8 @@ def passive_scan_results(target_url):
 def get_cves_by_cwe(cwe_id, page_number=1, page_size=50):
     query = CVE.objects.filter(
         # weaknesses__contains=[{"description": [{"value": f"CWE-{cwe_id}"}]}] # this targets both primary and secondary
-        weaknesses__contains=[{"type": "Primary", "description": [{"value": f"CWE-{cwe_id}"}]}] # this targets only primary.
+        weaknesses__contains=[{"type": "Primary", "description": [{"value": f"CWE-{cwe_id}"}]}], # this targets only primary.
+        metrics__contains = {"cvssMetricV30": []}, # Ensures cvssMetricV30 exists
         )
     query = query.order_by("-cve_id")  # Sort in descending order by CVE ID
     paginator = Paginator(query, page_size)
